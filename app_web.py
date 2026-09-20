b4ce7a9c1044e2230415397ec6cb1c8025e7ead6
AbGithub2018 <35710115+AbGithub2018@users.noreply.github.com>
2026-09-19 23:29:33 -0400

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -75,9 +75,9 @@ if 'banniere_active' not in st.session_state:
 st.sidebar.html("<h2 style='color: #003366; font-family: sans-serif;'>📊 STATISTIQUES ET FILTRES</h2>")
 
 if 'entreprise_pays' in df.columns:
-    repartition_pays = df['entreprise_pays'].value_counts()
-    st.sidebar.write("**Origine financière de vos produits :**")
-    st.sidebar.bar_chart(repartition_pays)
+    #repartition_pays = df['entreprise_pays'].value_counts()
+    #st.sidebar.write("**Origine financière de vos produits :**")
+    #st.sidebar.bar_chart(repartition_pays)
 
 st.sidebar.markdown("---")
 

91104f0b04cb8409766903f1bdd2aa78616c4b22
AbGithub2018 <35710115+AbGithub2018@users.noreply.github.com>
2026-09-22 20:43:28 -0400

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -58,16 +58,14 @@ def charger_donnees():
 
 def sauvegarder_donnees(df_a_enregistrer):
     """Enregistre les prix automatiquement grâce aux secrets de Streamlit Cloud."""
-        try:
-        conn = st.connection("gsheets", type=GSheetsConnection)
-        conn.update(worksheet="Sheet1", data=df_a_enregistrer)
-        
-        # --- LES DEUX LIGNES MAGIQUES À AJOUTER ICI ---
-        st.cache_data.clear()
-        if 'df_produits' in st.session_state:
-            del st.session_state['df_produits']
-            
-        return True
+     try:
+         conn = st.connection("gsheets", type=GSheetsConnection)
+         conn.update(worksheet="Sheet1", data=df_a_enregistrer)
+         st.cache_data.clear()
+         if 'df_produits' in st.session_state:
+             del st.session_state['df_produits']
+         return True
+     
     except Exception as e:
         st.error(f"❌ Erreur de sauvegarde réelle : {e}")
         return False
